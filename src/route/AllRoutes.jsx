e1982b243c2c1ea9bee03fe8eb47fcab56c22f6f
himanshu-449 <himanshutanwar309@gmail.com>
2025-08-20 22:13:33 +0530

    Working

--- src/route/AllRoutes.jsx
+++ src/route/AllRoutes.jsx
@@ -1,23 +1,21 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-const MovieList = lazy(() => import("../pages/MovieList"));
-const MovieDetail = lazy(() => import("../pages/MovieDetail"));
-const Search = lazy(() => import("../pages/Search"));
-const PageNotFound = lazy(() => import("../pages/PageNotFound"));
+import {MovieList} from "../pages/MovieList";
+import {MovieDetail} from "../pages/MovieDetail";
+import {Search} from "../pages/Search";
+import {PageNotFound} from "../pages/PageNotFound";
 
 export const AllRoutes = () => (
   <div className="dark:bg-darkbg">
-    <Suspense fallback={null}>
-      <Routes>
-        <Route path="/" end element={<MovieList apiPath="movie/now_playing" title="Home" />} />
-        <Route path="movies/popular" element={<MovieList apiPath="movie/popular" title="Popular" />} />
-        <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated" />} />
-        <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming" />} />
-        <Route path="movie/:id" element={<MovieDetail />} />
-        <Route path="search" element={<Search apiPath="search/movie" />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path="/" end element={<MovieList apiPath="movie/now_playing" title="Home" />} />
+      <Route path="movies/popular" element={<MovieList apiPath="movie/popular" title="Popular" />} />
+      <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated" />} />
+      <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming" />} />
+      <Route path="movie/:id" element={<MovieDetail />} />
+      <Route path="search" element={<Search apiPath="search/movie" />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   </div>
 );
