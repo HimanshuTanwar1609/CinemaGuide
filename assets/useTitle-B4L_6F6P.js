546bf4f214036dcec76f1665a7be66def79da058
himanshu-449 <himanshutanwar309@gmail.com>
2025-08-21 00:29:37 +0530

    Updates

---
+++ assets/useTitle-B4L_6F6P.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-D605JF8d.js";const u="/CinemaGuide/assets/backup-vxh95_Dc.png",a=e=>(t.useEffect(()=>{document.title=`${e} / Cinema Guide`}),null);export{u as B,a as u};
--- assets/useTitle-BqOmM1-I.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-ZsDVrmde.js";const u="/CinemaGuide/assets/backup-vxh95_Dc.png",a=e=>(t.useEffect(()=>{document.title=`${e} / Cinema Guide`}),null);export{u as B,a as u};
