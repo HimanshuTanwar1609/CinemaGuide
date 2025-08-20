6cae77414c4b386292d8a93d03e3b3e3d9d12565
himanshu-449 <himanshutanwar309@gmail.com>
2025-08-20 19:21:22 +0530

    updated vite configure base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CinemaGuide/',  
+  base: '/',  // <-- set to root because HashRouter handles routing
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'
